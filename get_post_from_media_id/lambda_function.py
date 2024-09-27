38158dae88430b5dda08f34d7c767988013c5df3
Jolie Ni <ninan980805@gmail.com>
2024-09-26 22:37:13 -0400

    add function to fetch from instagram graph api

--- get_post_from_media_id/lambda_function.py
+++ get_post_from_media_id/lambda_function.py
@@ -10,27 +10,65 @@ import shutil
 import json
 import boto3
 from botocore.exceptions import ClientError
-from openai import OpenAI
 import re
 import requests
 
-def lambda_handler(event, context):
+def get_api_key(secret_name): 
+    region_name = "us-east-1"
 
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name,
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+    print(f"{secret_name}: {secret}")
+    return json.loads(secret)[secret_name]
+
+def get_caption_from_media_id(media_id):
+    instagram_access_token = get_api_key("INSTAGRAM_GRAPH_API")
+    instagram_graph_api = "https://graph.instagram.com/v20.0/" + media_id + "?fields=caption&access_token" + instagram_access_token
+    response = requests.get(instagram_graph_api)
+
+    if response.status_code == 200:
+      data = response.json()
+      return data["caption"]
+    else:
+      print(f"Failed to fetch from instagram graph API: {response.status_code}")
+      return None
+
+def lambda_handler(event, context):
+    print(event)
+    sqs = boto3.client('sqs')
+    queue_url = "https://sqs.us-east-1.amazonaws.com/310780496713/openai_api.fifo"
     records = event["Records"]
     for record in records:
         body = record["body"]
         print(body)
         bodyJson = json.loads(body)
-        shortCode = bodyJson["shortCode"]
-        print("sender: " + bodyJson["sender"] + ", shortCode: " + shortCode)
-        post = Post.from_shortcode(L.context, shortCode)
-        print(post.caption)
-        addresses, businessNames = get_address(post.caption)
-        if len(addresses) != len(businessNames):
-            print("Error: Addresses and Name length mismatched")
-        else:
-            for i in range(len(addresses)):
-                verified_address = cross_verify_address(business_name=businessNames[i], business_address=addresses[i])
-                write_to_DB(bodyJson["requestId"] + ':' + str(i), bodyJson["sender"], shortCode, businessNames[i] , verified_address)
+        sender = bodyJson["sender"]
+        media_id = bodyJson["media_id"]
+        mid = bodyJson["mid"]
+        print("sender: " + sender + ", media_id: " + media_id)
+        # get post from instagram graph api
+        caption = get_caption_from_media_id(media_id)
+        if (caption == None):
+            return
+        
+        message_body = {
+            "mid": mid,
+            "sender": sender,
+            "caption": caption
+        }
 
-    print(event)
+        sqs.sendMessage(queue_url, json.dump(message_body))
